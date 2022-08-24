dfe0dab35a349518f881dfb3545086e7f62c44a5
amar-jay <abdelmanan.abdelrahman03@gmail.com>
2022-08-24 18:57:40 +0300

    Removed NeoSolrized.vim

--- lua/amarjay/set.lua
+++ lua/amarjay/set.lua
@@ -30,6 +30,12 @@ vim.opt.backup = false
 vim.opt.path:append { '**' } -- In finding files
 vim.opt.cmdheight = 1 -- Give more space for displaying messages. 
 vim.opt.inccommand = 'split'
+vim.opt.cursorline = true
+vim.opt.termguicolors = true
+vim.opt.winblend = 0
+vim.opt.wildoptions = 'pum'
+vim.opt.pumblend = 5
+vim.opt.background = 'dark'
 
 -- Having longer updatetime (default is 4000 ms = 4 s) leads to noticeable
 -- delays and poor user experience.
