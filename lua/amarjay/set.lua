b7d86f640313607e5d58944613662bd9d387bacd
amar-jay <abdelmanan.abdelrahman03@gmail.com>
2022-09-11 11:42:21 +0300

    minor changes to vim default set

--- lua/amarjay/set.lua
+++ lua/amarjay/set.lua
@@ -15,8 +15,8 @@ vim.opt.showcmd = true
 vim.opt.backup = false -- No backup of files
 vim.opt.cmdheight = 1
 vim.opt.laststatus = 2
-vim.opt.scrolloff = 8
-vim.opt.shell = 'neovim'
+vim.opt.scrolloff = 10
+vim.opt.shell = neovim
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 vim.opt.softtabstop = 2
@@ -24,7 +24,7 @@ vim.opt.smarttab = true
 vim.opt.shiftwidth = 2
 vim.opt.ignorecase = true -- case insensitive unless /C
 vim.opt.wrap = false
--- vim.opt.backspace = { 'start', 'eol', 'indent'}
+vim.opt.backspace = { 'start', 'eol', 'indent'}
 vim.opt.swapfile = false
 vim.opt.backup = false
 vim.opt.path:append { '**' } -- In finding files
@@ -36,12 +36,17 @@ vim.opt.winblend = 0
 vim.opt.wildoptions = 'pum'
 vim.opt.pumblend = 5
 vim.opt.background = 'dark'
+vim.opt.signcolumn = "yes"
+--vim.opt.isfname:append("@-@")
 
 -- Having longer updatetime (default is 4000 ms = 4 s) leads to noticeable
 -- delays and poor user experience.
-vim.opt.updatetime = 50
+vim.opt.updatetime = 4000
 
-
+--vim.opt.colorcolumn = "800"
 vim.g.mapleader = " " -- Set Leader
 
-vim.opt.clipboard:prepend { 'unnamed', 'unnamedplus' }
+-- Do not pass emssage to |ins-completion-menu|
+vim.opt.shortmess:append("c")
+
+vim.opt.clipboard = 'unnamed'
