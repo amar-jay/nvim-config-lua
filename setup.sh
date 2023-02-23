b2697bb89b90cd24ca4ecdc681bc43b9236af357
amar-jay <abdelmanan.abdelrahman03@gmail.com>
2023-02-24 00:48:36 +0300

    setup config for codespaces

--- setup.sh
+++ setup.sh
@@ -1,22 +1,23 @@
 # Neovim installation setup by app image
 echo "Neovim installation"
 #curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
-curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
-chmod u+x nvim.appimage
-./nvim.appimage --appimage-extract
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage 
+chmod u+x nvim.appimage && ./nvim.appimage --appimage-extract
 ./squashfs-root/AppRun --version
 
 # Optional: exposing nvim globally.
-sudo mv squashfs-root / 
-sudo ln -s /squashfs-root/AppRun /usr/bin/nvim
+sudo mv squashfs-root / &&
+sudo ln -s /squashfs-root/AppRun /usr/bin/nvim &&
 nvim --version
 
 # Installing Packer , a lua package manager
 echo "Packer install"
 git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
-
-# This contains tmux and neovim config files
 echo "cloning amar-jay/nvim-config-lua"
-git clone https://github.com/amar-jay/.dotfiles $HOME/.config
+# This contains tmux and neovim config files
+git clone https://github.com/amar-jay/nvim-config-lua $HOME/.config/tmp
+mv $HOME/.config/tmp/* $HOME/.config/nvim/
+mv $HOME/.config/tmp/.* $HOME/.config/nvim/
+rmdir tmp 
 
-nvim
+nvim --version
