c61d0808dcf1b12c6e2d83315371f9a4485847db
amar-jay <abdelmanan.abdelrahman03@gmail.com>
2023-02-24 00:36:19 +0300

    setup config for codespaces

--- setup.sh
+++ setup.sh
@@ -2,10 +2,11 @@
 echo "Neovim installation"
 #curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
-chmod u+x nvim.appimage \
-./nvim.appimage --appimage-extract && ./squashfs-root/AppRun --version
+chmod u+x nvim.appimage
+./nvim.appimage --appimage-extract
+./squashfs-root/AppRun --version
 
-## Optional: exposing nvim globally.
+# Optional: exposing nvim globally.
 sudo mv squashfs-root / 
 sudo ln -s /squashfs-root/AppRun /usr/bin/nvim
 nvim --version
@@ -15,7 +16,7 @@ echo "Packer install"
 git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 
 # This contains tmux and neovim config files
-echo "cloning amar-jay/.dotfiles"
+echo "cloning amar-jay/nvim-config-lua"
 git clone https://github.com/amar-jay/.dotfiles $HOME/.config
 
 nvim
